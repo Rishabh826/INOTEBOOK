91d34ad6ea716aac4def35dae1b152033694e0a0
Rishabh826 <rishabhjangid0707@gmail.com>
2024-03-23 14:04:12 +0530

    salting of password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const { body, validationResult } = require('express-validator');
-
+var bcrypt = require('bcryptjs');
 router.post('/',[
  body('Name').isLength({min:3}),
  body('username').isLength({min:5}),
@@ -18,9 +18,12 @@ router.post('/',[
         if(existingUser){
             return res.status(400).json({error: "Sorry, user already exists."});
         }
+        const salt = await bcrypt.genSalt(10);
+        const hashpass = await bcrypt.hashSync(req.body.password, salt);
         let newUser = await User.create({
             name: req.body.name,
-            email: req.body.email,
+            username: req.body.username,
+            email: hashpass,
             password: req.body.password,
         });
         res.status(201).json(newUser);
